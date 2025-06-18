84f94c6c745ed247eba03f1b511b7fbc7e16de13
Mykhailo Budiakov <mishabudjakov@gmail.com>
2025-06-18 18:47:37 +0300

    V13 getSceneControlButtons

--- module/control.ts
+++ module/control.ts
@@ -1,17 +1,18 @@
-import { getSetting, setSetting } from './settings.js';
+import {getSetting, setSetting} from './settings.js';
 
-function addControl(sceneControls: any[]) {
-	if (!game.user!.isGM) return;
-	const tokenControl = sceneControls.find((c) => c.name === 'token')!;
+function addControl(sceneControls: { tokens: { tools: {} } }) {
+    if (!game.user!.isGM) return;
 
-	tokenControl.tools.push({
-		name: 'size-snap',
-		title: 'Size Snap',
-		icon: 'fas fa-diagram-venn',
-		toggle: true,
-		active: getSetting('snapTokens'),
-		onClick: (toggled: boolean) => setSetting('snapTokens', toggled),
-	});
+    sceneControls.tokens.tools.sizeSnap = {
+        name: 'sizeSnap',
+        title: 'Size Snap',
+        icon: 'fas fa-diagram-venn',
+        toggle: true,
+        active: getSetting('snapTokens'),
+        onChange: (event, toggled: boolean) => {
+            setSetting('snapTokens', toggled)
+        },
+    }
 }
 
 Hooks.on('getSceneControlButtons', addControl);

9427b66cbc677c2a69842bb0fb240cc3eb7eef72
Elizeu Angelo <elizeu90@gmail.com>
2026-02-23 08:41:21 -0300

    Refactor settings and control logic to include hide button option for size snap feature

--- module/control.ts
+++ module/control.ts
@@ -2,14 +2,15 @@ import { getSetting, setSetting } from './settings.js';
 
 function addControl(sceneControls: { tokens: { tools: { sizeSnap: any } } }) {
 	if (!game.user!.isGM) return;
+	if (getSetting('hideBtn')) return;
 
 	sceneControls.tokens.tools.sizeSnap = {
 		name: 'sizeSnap',
-		title: game.i18n.localize("z-scatter.sizeSnap.title"),
+		title: game.i18n.localize('z-scatter.sizeSnap.title'),
 		icon: 'fas fa-diagram-venn',
 		toggle: true,
 		active: getSetting('snapTokens'),
-		onChange: (event, toggled: boolean) => {
+		onChange: (_event, toggled: boolean) => {
 			setSetting('snapTokens', toggled);
 		},
 	};
--- module/settings.ts
+++ module/settings.ts
@@ -1,4 +1,4 @@
-export const SYSTEM_ID = 'z-scatter';
+export const MODULE_ID = 'z-scatter';
 
 const settings = {
 	snapTokens: {
@@ -9,9 +9,26 @@ const settings = {
 		default: true,
 		onChange: () => canvas.tokens!.placeables.forEach((t) => t.refresh()),
 	},
+	hideBtn: {
+		name: `${MODULE_ID}.settings.hideBtn.name`,
+		hint: `${MODULE_ID}.settings.hideBtn.hint`,
+		scope: 'world',
+		config: true,
+		type: Boolean,
+		default: false,
+		requiresReload: true,
+	},
+	ignoreDead: {
+		name: `${MODULE_ID}.settings.ignoreDead.name`,
+		hint: `${MODULE_ID}.settings.ignoreDead.hint`,
+		scope: 'world',
+		config: true,
+		type: Boolean,
+		default: true,
+	},
 	scatter: {
-		name: `${SYSTEM_ID}.settings.scatter.name`,
-		hint: `${SYSTEM_ID}.settings.scatter.hint`,
+		name: `${MODULE_ID}.settings.scatter.name`,
+		hint: `${MODULE_ID}.settings.scatter.hint`,
 		scope: 'world',
 		config: true,
 		type: Number,
@@ -22,28 +39,20 @@ const settings = {
 			step: 0.01,
 		},
 	},
-	ignoreDead: {
-		name: `${SYSTEM_ID}.settings.ignoreDead.name`,
-		hint: `${SYSTEM_ID}.settings.ignoreDead.hint`,
-		scope: 'world',
-		config: true,
-		type: Boolean,
-		default: true,
-	},
 } as const;
 
 export type Settings = typeof settings;
 
 export function getSetting<T extends keyof Settings>(name: T) {
-	return game.settings.get(SYSTEM_ID, name) as unknown as ReturnType<Settings[T]['type']>;
+	return game.settings.get(MODULE_ID, name) as unknown as ReturnType<Settings[T]['type']>;
 }
 
 export function setSetting<T extends keyof Settings>(name: T, value: ReturnType<Settings[T]['type']>) {
-	return game.settings.set(SYSTEM_ID, name, value);
+	return game.settings.set(MODULE_ID, name, value);
 }
 
 Hooks.once('setup', () => {
 	for (const [key, setting] of Object.entries(settings)) {
-		game.settings.register(SYSTEM_ID, key, setting as unknown as any);
+		game.settings.register(MODULE_ID, key, setting as unknown as any);
 	}
 });
